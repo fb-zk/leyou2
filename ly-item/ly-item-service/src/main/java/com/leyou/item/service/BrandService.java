cfc76d193c6e63f070b239925cae1a5ae26f50b0
fb <fb@zhangkai.com>
2019-12-02 15:12:39 +0800

    分页条件查询品牌

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -0,0 +1,50 @@
+package com.leyou.item.service;
+
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.common.vo.PageResult;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.pojo.Brand;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class BrandService {
+    @Resource
+    private BrandMapper brandMapper;
+
+
+    public PageResult<Brand> queryBrandByPage(Integer page, Integer rows, String sortBy, Boolean desc, String key) {
+        //开始分页
+        PageHelper.startPage(page,rows);
+        Example example = new Example(Brand.class);
+        Example.Criteria  criteria= example.createCriteria();
+        //关键词过滤
+        if (StringUtils.isNotBlank(key)){
+            criteria.orLike("name", "%" + key + "%").
+                    orEqualTo("letter", key.toUpperCase());
+        }
+        //排序
+        if (StringUtils.isNotBlank(sortBy)){
+            String orderByClause = sortBy + (desc ? " DESC": " ASC" );
+            example.setOrderByClause(orderByClause);
+        }
+        //查询
+        List<Brand> brands = brandMapper.selectByExample(example);
+        if (CollectionUtils.isEmpty(brands)){
+            throw  new LyException(ExceptionEnum.BRAND_NOT_FOUND);
+        }
+        //解析结果
+        PageInfo<Brand> pageInfo = new PageInfo<>(brands);
+        return new PageResult<>(pageInfo.getTotal(),pageInfo.getList());
+    }
+}
